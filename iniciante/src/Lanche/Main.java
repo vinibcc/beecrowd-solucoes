20f570812eac12569e9eef227c188cf9755c1edd
vinibcc <marcos.isidorio@ufape.edu.br>
2025-11-18 15:10:04 -0300

    Resolução do problema 1038 - Lanche

---
+++ iniciante/src/Lanche/Main.java
@@ -0,0 +1,14 @@
+package Lanche;
+
+import java.util.Scanner;
+
+public class Lanche {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double[] precos = {4.00, 4.50, 5.00, 2.00, 1.50};
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+        double total = quantidade * precos[codigo - 1];
+        System.out.printf("Total: R$ %.2f\n", total);
+    }
+}
